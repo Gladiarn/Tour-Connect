6c34f5077d7e01dd22d17037584bf3366cf32bfc
Ianne <bulilaniannecarl@gmail.com>
2025-06-12 20:21:29 +0800

    11-PaddingFixandcolor

--- src/components/Destinations/Destinations.tsx
+++ src/components/Destinations/Destinations.tsx
@@ -7,7 +7,7 @@ export default function Destinations() {
   return (
     <div className="flex flex-col w-full h-fit bg-white">
       <div
-        className="flex flex-col w-full items-center justify-center h-fit gap-[30px] pl-[300px] pt-[130px] pr-[300px] pb-[20px]"
+        className="flex flex-col w-full items-center justify-center h-fit gap-[30px] pl-[150px] pt-[130px] pr-[150px] pb-[20px]"
         style={{
           backgroundImage: "url('/images/des.png')",
           backgroundSize: "100%", // or 100%, 90%, etc.
@@ -16,7 +16,7 @@ export default function Destinations() {
         }}
       >
         <p className="flex font-bold text-[64px] text-white">Destinations</p>
-        <div className="flex w-full font-black h-fit gap-[30px] px-[10px] items-center justify-center py-[10px]">
+        <div className="flex w-full text-black h-fit gap-[30px] px-[10px] items-center justify-center py-[10px]">
           <div className="flex items-center justify-between bg-white py-[10px] px-[10px] rounded-[10px] w-full h-fit font-medium text-[20px]">
             Province
             <button className="flex items-center cursor-pointer">
@@ -37,7 +37,7 @@ export default function Destinations() {
           </div>
         </div>
       </div>
-      <div className="flex w-full h-fit bg-white gap-[30px] flex-wrap items-center justify-center px-[180px] py-[20px]">
+      <div className="flex w-full h-fit bg-white gap-[30px] flex-wrap items-center justify-center px-[90px] py-[20px]">
         <div className="flex flex-col w-[525px] h-[307px]">
           <div
             className="flex w-full rounded-t-[10px] h-[173px]"
